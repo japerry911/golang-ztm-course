ca2a7beae6084b5c328956d411445d78e7f151b2
Jack Perry <jack.e.perry@outlook.com>
2022-10-23 11:46:40 -0600

    👌 IMPROVE: looping demo done

--- course-content/src/lectures/demo/loops/loops.go
+++ course-content/src/lectures/demo/loops/loops.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	sum := 0
+	fmt.Println("sum =", sum)
 
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("sum =", sum)
+	}
+
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("sum =", sum)
+	}
 }
