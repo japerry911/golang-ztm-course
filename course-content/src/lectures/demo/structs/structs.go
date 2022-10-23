ee69638bfa3aa527531972808abe87907d3b641c
Jack Perry <jack.e.perry@outlook.com>
2022-10-23 12:41:22 -0600

    👌 IMPROVE: structs demo done

--- course-content/src/lectures/demo/structs/structs.go
+++ course-content/src/lectures/demo/structs/structs.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	Boarded      bool
+}
 
+type Bus struct {
+	Frontseat Passenger
+}
+
+func main() {
+	casey := Passenger{"Casey", 1, false}
+	fmt.Println("casey", casey)
+
+	var (
+		bill = Passenger{Name: "Bill", TicketNumber: 2}
+		ella = Passenger{Name: "Ella", TicketNumber: 3}
+	)
+	fmt.Println("bill", bill, "ella", ella)
+
+	var heidi Passenger
+	heidi.Name = "Heidi"
+	heidi.TicketNumber = 4
+	fmt.Println("heidi", heidi)
+
+	heidi.Boarded = true
+	bus := Bus{Frontseat: heidi}
+	fmt.Println("bus", bus)
 }
