db81e593c4c924872389dbaa8952c2db9839c2bb
Jack Perry <jack.e.perry@outlook.com>
2022-10-23 10:51:09 -0600

    👌 IMPROVE: changed name

---
+++ course-content/src/lectures/demo/variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myName = "Jack"
+	fmt.Println("My name is", myName)
+
+	var name string = "Skylord"
+	fmt.Println("My name is", name)
+
+	userName := "jasky"
+	fmt.Println("My username is", userName)
+
+	var sum int
+	fmt.Println("The sum is", sum)
+
+	part1, other := 1, 5
+	fmt.Println("part1", part1, "|| other", other)
+}
---
+++ course-content/src/lectures/exercise/variables/variables.go
@@ -0,0 +1,44 @@
+//Summary:
+//  Print basic information to the terminal using various variable
+//  creation techniques. The information may be printed using any
+//  formatting you like.
+//
+//Requirements:
+//* Store your favorite color in a variable using the `var` keyword
+//* Store your birth year and age (in years) in two variables using
+//  compound assignment
+//* Store your first & last initials in two variables using block assignment
+//* Declare (but don't assign!) a variable for your age (in days),
+//  then assign it on the next line by multiplying 365 with the age
+// 	variable created earlier
+//
+//Notes:
+//* Use fmt.Println() to print out information
+//* Basic math operations are:
+//    Subtraction    -
+// 	  Addition       +
+// 	  Multiplication *
+// 	  Division       /
+
+package main
+
+import "fmt"
+
+func main() {
+	favoriteColor := "purple"
+	birthYear, age := 1995, 27
+	var (
+		firstInitial rune = 'J'
+		lastInitial  rune = 'P'
+	)
+	var ageInDays int
+
+	ageInDays = age * 365
+
+	fmt.Println("favoriteColor", favoriteColor)
+	fmt.Println("birthYear", birthYear)
+	fmt.Println("age", age)
+	fmt.Println("firstInitial", firstInitial)
+	fmt.Println("lastInitial", lastInitial)
+	fmt.Println("ageInDays", ageInDays)
+}
