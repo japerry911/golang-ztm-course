ce312418964fbcd09b0e1cced34ce5d9cd6c321a
Jack Perry <jack.e.perry@outlook.com>
2022-10-23 11:10:15 -0600

    👌 IMPROVE: functionss exercise done

--- course-content/src/lectures/exercise/functions/functions.go
+++ course-content/src/lectures/exercise/functions/functions.go
@@ -19,6 +19,31 @@ package main
 
 import "fmt"
 
-func main() {
+func greetPerson(name string) {
+	fmt.Println("greetings", name)
+}
 
+func aMessage() string {
+	return "this is a message"
+}
+
+func add3Ints(int1, int2, int3 int) int {
+	return int1 + int2 + int3
+}
+
+func returnInt() int {
+	return 32
+}
+
+func return2Ints() (int, int) {
+	return 3, 2
+}
+
+func main() {
+	greetPerson("Skylord")
+	fmt.Println(aMessage())
+	fmt.Println("1 + 2 + 3 =", add3Ints(1, 2, 3))
+	fmt.Println("returnInt", returnInt())
+	int1, int2 := return2Ints()
+	fmt.Println("return2Ints =", int1, int2)
 }
