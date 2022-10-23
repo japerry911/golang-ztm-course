c3549f51b9ea8abfb2b38faf7af655c797fa51ba
Jack Perry <jack.e.perry@outlook.com>
2022-10-23 11:21:23 -0600

    👌 IMPROVE: if-else demo done

--- course-content/src/lectures/demo/if-else/if-else.go
+++ course-content/src/lectures/demo/if-else/if-else.go
@@ -8,5 +8,19 @@ func average(a, b, c int) float32 {
 }
 
 func main() {
+	quiz1, quiz2, quiz3 := 9, 7, 8
 
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 > quiz2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("quiz1 < quiz2")
+	} else {
+		fmt.Println("quiz1 == quiz2")
+	}
+
+	if average(quiz1, quiz2, quiz3) > 7 {
+		fmt.Println("acceptable average score (> 7)")
+	} else {
+		fmt.Println("unacceptable average scores (<= 7)")
+	}
 }
