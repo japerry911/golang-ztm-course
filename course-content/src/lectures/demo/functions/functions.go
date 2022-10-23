341e4fa54fa78495dbbba168176900a1edd8d8b8
Jack Perry <jack.e.perry@outlook.com>
2022-10-23 11:02:19 -0600

    👌 IMPROVE: functions demo done

--- course-content/src/lectures/demo/functions/functions.go
+++ course-content/src/lectures/demo/functions/functions.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
+func double(x int) int {
+	return x + x
+}
 
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func greet() {
+	fmt.Println("Hello from greet func")
+}
+
+func main() {
+	greet()
+
+	dozen := double(6)
+	fmt.Println("dozen", dozen)
+
+	bakersDozen := add(dozen, 1)
+	fmt.Println("bakersDozen", bakersDozen)
 }
