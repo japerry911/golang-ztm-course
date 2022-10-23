f985d2291aebf46b4dd7c6e7c8f6e8b86f3db79f
Jack Perry <jack.e.perry@outlook.com>
2022-10-23 14:17:16 -0600

    👌 IMPROVE: arrays demo done

--- course-content/src/lectures/demo/arrays/arrays.go
+++ course-content/src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is not clean")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "office"},
+		{name: "warehouse"},
+		{name: "reception"},
+		{name: "ops"},
+	}
+
+	checkCleanliness(rooms)
+
+	fmt.Println("Performing cleaning...")
+
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanliness(rooms)
 }
