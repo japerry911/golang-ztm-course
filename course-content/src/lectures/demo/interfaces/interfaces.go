a3efcfd4ba888a6af3178b0a4de1ce9613495752
Jack Perry <jack.e.perry@outlook.com>
2022-10-25 08:14:10 -0600

    👌 IMPROVE: interfaces demo completed

--- course-content/src/lectures/demo/interfaces/interfaces.go
+++ course-content/src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+	PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("cook salad")
+}
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("--Dish: %v--\n", dish)
+		dish.PrepareDish()
+	}
+
+	fmt.Println()
+}
+
+func main() {
+	dishes := []Preparer{Chicken("Chicken"), Salad("Caesar")}
+	prepareDishes(dishes)
 }
